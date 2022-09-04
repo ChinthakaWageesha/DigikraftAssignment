01b58ff4485d8df19a21888e11ce181efdfb906a
Wageesha Chinthaka <wageesha.chinthaka@myahmed.com>
2022-09-04 20:03:59 +0530

    Bike Stations Main Page UI implemented with dummy data

---
+++ app/src/main/java/com/example/digikraftassignment/app/presentation/BikeStationAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.digikraftassignment.app.presentation
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.RecyclerView
+import com.example.digikraftassignment.R
+import com.example.digikraftassignment.core.extension.showToast
+
+class BikeStationAdapter(
+    private val context: Context
+) : RecyclerView.Adapter<BikeStationAdapter.BikeStationViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BikeStationViewHolder {
+        return BikeStationViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_bike_station, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: BikeStationViewHolder, position: Int) = holder.run {
+        onBind(position)
+    }
+
+    override fun getItemCount(): Int = 10
+
+    inner class BikeStationViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private val clStationBase = itemView.findViewById<ConstraintLayout>(R.id.cl_station_base)
+        private val txtStationName = itemView.findViewById<TextView>(R.id.tv_bike_station_name)
+        private val txtDistance = itemView.findViewById<TextView>(R.id.tv_bike_station_distance)
+        private val txtBikeStation = itemView.findViewById<TextView>(R.id.tv_title_bike_station)
+        private val txtAvailableBikes = itemView.findViewById<TextView>(R.id.tv_available_bikes)
+        private val txtAvailablePlaces = itemView.findViewById<TextView>(R.id.tv_available_places)
+
+        @SuppressLint("SetTextI18n")
+        fun onBind(position: Int) {
+
+            txtStationName.text = "15589 Ofiar Dabia"
+            txtDistance.text = "800m"
+            txtBikeStation.text = context.getString(R.string.label_bike_station)
+            txtAvailableBikes.text = "7"
+            txtAvailablePlaces.text = "21"
+
+            clStationBase.setOnClickListener { "Clicked".showToast(context) }
+
+        }
+    }
+}
--- app/src/main/java/com/example/digikraftassignment/app/presentation/MainActivity.kt
+++ app/src/main/java/com/example/digikraftassignment/app/presentation/MainActivity.kt
@@ -2,13 +2,26 @@ package com.example.digikraftassignment.app.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.digikraftassignment.R
 import com.example.digikraftassignment.app.dependencyinjection.injectFeature
+import com.example.digikraftassignment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         injectFeature()
+        setBikeStationAdapter()
     }
+
+    private fun setBikeStationAdapter() {
+        binding.rvBikeStation.adapter = BikeStationAdapter(applicationContext)
+        binding.rvBikeStation.layoutManager = LinearLayoutManager(applicationContext)
+    }
+
 }
